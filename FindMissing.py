266fcff3022b580f776fda075f352b3b346e9850
Electricjacob11 <electricjacob11@gmail.com>
2025-09-30 19:25:25 -0700

    a

--- FindMissing.py
+++ FindMissing.py
@@ -6,7 +6,7 @@ checks = [False] * (length + 1)
 for i in range(0, len(linst)):
     checks[linst[i]-1] = True
 
-for i in range(0, len(checks)):
+for i in range(0, length + 1):
     if checks[i] == False:
         print(i+1)
         break
