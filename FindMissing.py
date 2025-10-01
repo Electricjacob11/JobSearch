2fece21a0d36a2e4fb9cbaf581a1ee9702cf1508
Electricjacob11 <electricjacob11@gmail.com>
2025-09-30 19:24:56 -0700

    Find missing with check list

--- FindMissing.py
+++ FindMissing.py
@@ -1,15 +1,12 @@
-def quicksort(arr):
-    if len(arr) <= 1:
-        return arr
-    pivot = arr[len(arr) // 2]
-    left = [x for x in arr if x < pivot]
-    middle = [x for x in arr if x == pivot]
-    right = [x for x in arr if x > pivot]
-    return quicksort(left) + middle + quicksort(right)
+linst = [2, 1, 3, 5]
 
+length = len(linst)
+checks = [False] * (length + 1)
 
-list = quicksort([3,6,8,10,1,2,1])
+for i in range(0, len(linst)):
+    checks[linst[i]-1] = True
 
-for i in range(len(list)-1):
-    if list[i] + 1 != list[i+1]:
-        print(f"Missing number between {list[i]} and {list[i+1]}: {list[i]+1}")
+for i in range(0, len(checks)):
+    if checks[i] == False:
+        print(i+1)
+        break
