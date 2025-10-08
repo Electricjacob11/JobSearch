eb539af673be56aed1260c58d7c5f618e60c657a
Electricjacob11 <electricjacob11@gmail.com>
2025-10-07 19:15:02 -0700

    Linst list

--- FindMissing.py
+++ FindMissing.py
@@ -1,20 +1,20 @@
 list = [2, 1, 5, 4]
 
-def find_missing_number(linst):
-    length = len(linst)
+def find_missing_number(list):
+    length = len(list)
     checks = [False] * (length + 1)
 
-    for i in range(0, len(linst)):
-        checks[linst[i]-1] = True
+    for i in range(0, len(list)):
+        checks[list[i]-1] = True
 
     for i in range(0, length + 1):
         if checks[i] == False:
             print(i+1)
             break
 
-def findmissingnum2(linst):
-    length = len(linst)
-    return ((length + 1) * (length + 2) // 2) - sum(linst)
+def findmissingnum2(list):
+    length = len(list)
+    return ((length + 1) * (length + 2) // 2) - sum(list)
 
 def main():
     find_missing_number(list)
