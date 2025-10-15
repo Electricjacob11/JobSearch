46a489a64470de9ad74e7d49e912ac9d47b03317
Electricjacob11 <electricjacob11@gmail.com>
2025-10-14 20:26:18 -0700

    A

--- TwoSum.py
+++ TwoSum.py
@@ -4,6 +4,7 @@ def twoSum(self, nums: List[int], target: int) -> List[int]:
     num_to_index = {} # Dictionary/hash map to store number and its index, automatically stores the index of the first occurrence of each number.
     
     # Go through the list and check to see if we've seen the complement before
+    # Enumerate allows for using both the index and the value of each number in the list.
     for i, num in enumerate(nums):
         complement = target - num # Check to see what the other number needed to reach the desired sum is.
         if complement in num_to_index:
