3248df16654b268b121d800e11e479d87100ddb4
Electricjacob11 <electricjacob11@gmail.com>
2025-10-14 20:22:11 -0700

    TwoSum

---
+++ TwoSum.py
@@ -0,0 +1,17 @@
+from typing import List
+
+def twoSum(self, nums: List[int], target: int) -> List[int]:
+    num_to_index = {}
+    
+    for i, num in enumerate(nums):
+        complement = target - num
+        if complement in num_to_index:
+            return [num_to_index[complement], i]
+        num_to_index[num] = i
+    
+    return []
+
+# Example usage:
+nums = [2, 8, 7, 15]
+target = 9
+print(twoSum(None, nums, target))
